b8739367e71e14fdfc9ff9d7ceb14f069569b3aa
esaurez <enrique.saurez@gmail.com>
2016-12-11 21:44:44 -0500

    Update version of g++

--- install_library.sh
+++ install_library.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Installing g++-4.9"
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get -y install g++-4.9
-
 echo "Installing Casablanca"
-sudo apt-get install -y libxml++2.6-dev libxml++2.6-doc uuid-dev g++ git make libboost-all-dev libssl-dev cmake
+sudo apt-get install -y libxml++2.6-dev libxml++2.6-doc uuid-dev g++ git make libboost-all-dev libssl-dev
 mkdir -p src
 cd ~/src
 git clone https://github.com/Microsoft/cpprestsdk.git casablanca
 cd casablanca/Release
 mkdir build.release
 cd build.release
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-4.9
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-4.8
 make
 sudo make install
 sudo ldconfig
@@ -34,7 +29,7 @@ git clone https://github.com/Azure/azure-storage-cpp.git
 cd azure-storage-cpp/Microsoft.WindowsAzure.Storage
 mkdir build.release
 cd build.release
-CASABLANCA_DIR=/usr/local/ CXX=g++-4.9 cmake .. -DCMAKE_BUILD_TYPE=Release
+CASABLANCA_DIR=/usr/local/ CXX=g++-4.8 cmake .. -DCMAKE_BUILD_TYPE=Release
 make
 #Install library
 sudo cp ~/src/azure-storage-cpp/Microsoft.WindowsAzure.Storage/build.release/Binaries/* /usr/local/lib
